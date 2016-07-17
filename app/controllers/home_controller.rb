948b8a71a1b518df4474150de8832201a16b3cfe
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-07-17 15:31:07 -0400

    add bootstrap with async requests in navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-
+    if request.xhr?
+      render layout: false
+    else
+      200
+    end
   end
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,7 @@
 class SkillsController < ApplicationController
   def index
-
+    if request.xhr?
+      render layout: false
+    end
   end
 end
