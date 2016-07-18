24b893c8a23955082a906b914fea6a585d9fa3a9
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-07-18 14:05:29 -0400

    add skills graph system, skill model and validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Skill.create([
+  {skill_name: "Ruby", skill_level: 5},
+  {skill_name: "JavaScript", skill_level: 4},
+  {skill_name: "HTML", skill_level: 5},
+  {skill_name: "CSS", skill_level: 5},
+  {skill_name: "Sqlite", skill_level: 3},
+  {skill_name: "Markdown", skill_level: 5},
+  {skill_name: "Ruby on Rails", skill_level: 5},
+  {skill_name: "PostgreSQL", skill_level: 4},
+  {skill_name: "Sinatra", skill_level: 5},
+  {skill_name: "Phaser.js", skill_level: 3},
+  {skill_name: "jQuery", skill_level: 4},
+  {skill_name: "Rspec", skill_level: 5},
+  {skill_name: "Jasmine", skill_level: 3},
+  {skill_name: "Capybara", skill_level: 4},
+  {skill_name: "React", skill_level: 2},
+  {skill_name: "AngularJS", skill_level: 3},
+  {skill_name: "Ionic", skill_level: 3},
+  ])
