af2b7c6ba6df149187ac1ce4a62e0242364f5844
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2016-07-20 11:19:31 -0400

    add color change background, project style changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,25 +30,24 @@ Skill.create([
 
 Project.create([
                 {name: "Pick",
-                description: "Pick is an app that allows local musicians to connect with one another based on musical talents, preferred genre, and location. The app's front end was built in AngularJS and Ionic, and it provides an Android and iOS compatible user interface. The back end is built in Rails 4, and uses a zipcode API and Soundcloud's API for location filtering and user profile information. The back-end was tested with RSpec.",
+                description: "Pick is an app that allows local musicians to connect with one another based on musical talents, preferred genre, and location."+"\n"+"The app's front end was built in AngularJS and Ionic, and it provides an Android and iOS compatible user interface."+"\n"+"The back end is built in Rails 4, and uses a zipcode API and Soundcloud's API for location filtering and user profile information. The back-end was tested with RSpec.",
                 url: "https://github.com/wesleye2003/pick-mobile",
                 image_url: "https://dl.dropboxusercontent.com/u/35780054/icon.png"},
                {name: "Donut Knight",
                description: "Donut Knight is a Zelda-style game built in Phaser.js and HTML5. The game is currently in development,
-               but this is a link to a sample fight from the game. The player moves around the screen with the arrow keys and
+               but this is a link to a sample fight from the game."+"\n"+"The player moves around the screen with the arrow keys and
                shoot projectiles(donuts) with the spacebar to defeat slime enemies and the boss monster.",
                url: "http://donut-knight.herokuapp.com/",
                image_url: "https://dl.dropboxusercontent.com/u/35780054/Screen%20Shot%202016-07-19%20at%205.33.02%20PM%20(2).png"},
                {name: "Bored? Games!",
                description: "'Bored? Games!' is a web application that allows users to search through a database of board games
-               using varying criteria. Users can also save games to their libraries, add other users as friends,
-               give their favorite games 'points' to give them a positive rating, and view their friends' libraries of games.
-
+               using varying criteria."+"\n"+"Users can also save games to their libraries, add other users as friends,
+               give their favorite games 'points' to give them a positive rating, and view their friends' libraries of games."+"\n"+"
                The application is a Ruby on Rails program that that is hosted on Heroku and uses a PostgreSQL database that was populated using Nokogiri and the board game geek API.",
                url: "https://bored-games-arch.herokuapp.com/",
                image_url: "https://dl.dropboxusercontent.com/u/35780054/Screen%20Shot%202016-07-19%20at%207.37.57%20PM.png"},
                {name: "256",
-               description: "256 is a game based on Gabriele Cirulli's popular mathematics game, 2048. It was created using HTML5 and JavaScript,
+               description: "256 is a game based on Gabriele Cirulli's popular mathematics game, 2048."+"\n"+"It was created using HTML5 and JavaScript,
                and it has been fully tested using the Jasmine framework.",
                url: "https://github.com/wesleye2003/256"}
                ])
